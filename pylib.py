83ffdea3a29e62b11a635e7eb91ff20fdf8d7d4b
Yongxu Zhang <yxzhang.cu@gmail.com>
2013-01-10 14:23:31 -0500

    pylib

---
+++ pylib.py
@@ -0,0 +1,19 @@
+#1 list comprehension
+foo = [some_func(x) for x in range(1, 11) if not (x % 2)]
+#2 dict comprehension
+{n:n*n for n in range(5)}
+
+
+
+
+#n regex
+import re
+
+
+
+
+#itertools: count(start [, step]), cycle(iterable), repeat(element [, num_of_rep])
+
+#2 generator: a "function" used as a iterable in place of a list
+
+
