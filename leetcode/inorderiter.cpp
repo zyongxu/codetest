f1c9438a5cc6837f69d7abb94dc2804ec1dd8990
Yongxu Zhang <yxzhang.cu@gmail.com>
2013-01-16 11:37:48 -0500

    add iterately traverse a binary tree in-order

--- leetcode/inorderiter.cpp
+++ leetcode/inorderiter.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
-#include <stack>
-#include <vector>
-
-using namespace std;
-
+/*
+ * iterately in-order traverse a binary tree
+ */
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -11,41 +8,32 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-vector<int> inorderTraversal(TreeNode *root) {
-  if (!root)
-    return vector<int>();
-      
-  vector<int> ret;
-  stack<TreeNode*> nstack;
-  TreeNode *temp = root;
-  
-  while (temp->left) {
-    nstack.push(temp);
-    temp = temp->left;
-  }
-
-  while (!nstack.empty()) {
-    if (!temp->left) {
-        ret.push_back(temp->val);
-        if (temp->right) {
-          temp = temp->right;
-        } else {
-          temp = nstack.top();
-          ret.push_back(temp->val);
-          nstack.pop();
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        //always forget check the input!!!
+        if (!root)
+            return vector<int>();
+            
+        vector<int> ret;
+        stack<TreeNode*> nstack;
+        TreeNode *cur=root;
+        while(true) {
+            if(cur) {
+                // keep going left
+                nstack.push(cur);
+                cur = cur->left;
+            } else {
+                if (nstack.empty())
+                    break;
+                // if no "left" to go, print the top of stack (the parent of the NULL left)
+                cur = nstack.top();
+                nstack.pop();
+                ret.push_back(cur->val);
+                // then go right
+                cur=cur->right;
+            }
         }
-    } else {
-      nstack.push(temp);
-      temp = temp->left;
+        return ret;
     }
-  }
-  return ret;
-}
-
-
-int main() {
-  TreeNode *root = new TreeNode(5);
-  inorderTraversal(root);
-  
-  return 0;
-}
+};
