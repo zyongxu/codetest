69190505394272fea757bebd5cb718733b2ffa3f
Yongxu Zhang <yxzhang.cu@gmail.com>
2013-09-04 15:59:51 -0400

    generate both charts

--- mypython/gplot.py
+++ mypython/gplot.py
@@ -1,6 +1,35 @@
 import gviz_api
 
-page_template = """
+gviz_template = """
+<html>
+  <head>
+  <title>Static example</title>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+    <script type="text/javascript">
+      // Load the Visualization API and the chart package.
+      google.load("visualization", "1.0", {packages:["annotatedtimeline"]});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        %(jscode)s
+        var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
+        chart.draw(jscode_data);
+      }
+    </script>
+  </head>
+  <body>
+    <!--Div that will hold the pie chart-->
+    <H2>Price</H2>
+    <div id="chart_div" style="width: 1600px; height: 500px;"></div>
+  </body>
+</html>
+"""
+
+dygraph_template = """
 <html>
   <head>
   <title>Static example</title>
@@ -35,7 +64,7 @@ def drawChart(data_dic):
     gdata_table  = gviz_api.DataTable(chart_schema)
     data2pop = []
     for k, v in sorted(data_dic.iteritems()):
-        data2pop.append([k, v[0]])
+        data2pop.append([k, float(v[0])/10])
 
     gdata_table.LoadData(data2pop)
     json = gdata_table.ToJSon()
@@ -43,5 +72,10 @@ def drawChart(data_dic):
     # Creating a JavaScript code string
     jscode = gdata_table.ToJSCode("jscode_data")
 
-    # Putting the JS code and JSon string into the template
-    print page_template % {"jscode": jscode}
+    fdyg = open('dygraph.html', 'w');
+    fdyg.write(dygraph_template % {"jscode": jscode})
+    fdyg.close()
+
+    fgviz = open('googlechart.html', 'w');
+    fgviz.write(gviz_template % {"jscode": jscode})
+    fgviz.close()
