2309ab8c0057920b13151627d4f17e1e8827ced5
Yongxu Zhang <yongxu2011@gmail.com>
2014-10-31 16:53:32 -0400

    hanoi done

---
+++ haskell/hm1/towers_of_hanoi.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- Execrise 5: Recursivly Solving the Tower of Hanoi Puzzle
+
+-- type declaration, like typedef in C++
+type Peg = String
+type Move = (Peg, Peg)
+
+{- |
+   Given the number of discs and names for the three pegs, hanoi
+   should return a list of moves to be performed to move the stack of
+   discs from the first peg to the second
+   e.g. hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
+-}
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n x y z
+    | n < 1 = []
+    | n == 1 = [(x, y)]
+    | otherwise = (hanoi (n-1) x z y) ++ ((x, y) : (hanoi (n-1) z y x))
+
