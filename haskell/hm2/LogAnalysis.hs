f235c384da0a47ef50d713b618dde76a63e2733d
Yongxu Zhang <yongxu2011@gmail.com>
2014-11-06 22:48:46 -0500

    impl parseMessage

--- haskell/hm2/LogAnalysis.hs
+++ haskell/hm2/LogAnalysis.hs
@@ -1,12 +1,15 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 
+-- possible helpful functions: read, lines, words, unwords, take, drop, and .
 import Log
 
 buildMessage :: [String] -> LogMessage
-buildMessage 'I':xs = Info (read xs[1]) xs[2]
-buildMessage 'W':xs = Warning (read xs[1]) xs[2]
-buildMessage 'E':xs = (Error (read xs[1])) (read xs[2]) xs[3]
+buildMessage ("E":(x:(y:zs))) = LogMessage (Error (read x)) (read y) (unwords zs)
+buildMessage (x:(y:zs))
+    | x == "W" = LogMessage Warning (read y) (unwords zs)
+    | x == "I" = LogMessage Info (read y) (unwords zs)
+buildMessage xs = Unknown (unwords xs)
 
 parseMessage :: String -> LogMessage
-    parseMessage str = buildMessage (words str)
+parseMessage str = buildMessage (words str)
