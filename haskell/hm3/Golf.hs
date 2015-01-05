a82e73637f39ed6014a738e47c47f4e42522de11
Yongxu Zhang <yongxu2011@gmail.com>
2015-01-04 20:30:32 -0500

    add partial application in comments

--- haskell/hm3/Golf.hs
+++ haskell/hm3/Golf.hs
@@ -9,8 +9,15 @@ every n xs = case drop (n-1) xs of  -- drop is a total function
     (y:ys) -> y : every n ys
 
 {-
- - use backslash ('\') to indicate lambda parameters
- - e.g. \x y -> x + y  -- takes two variables and return the sum
+ - use backslash ('\') to indicate lambda expression
+ - e.g. (\x y -> x + y)  -- takes two variables and return the sum
 -}
 skips :: [a] -> [[a]]
 skips x = map (\n -> every n x) [1..length x]
+
+{- if we define the every function as "every xs n" (i.e. [a] -> Int -> [a]),
+ - then we can also use function partial application in the skips as:
+ -      skips x = map (every x) [1..length x]
+ - which is equivalent to
+ -      skips x = map (\n -> every x n) [1..length x]
+ -}
